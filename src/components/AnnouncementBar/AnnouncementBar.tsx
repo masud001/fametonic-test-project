75ea0b3a9b7e956f65b0108ceb32b3a506af60c0
masud001 <madhnagar@gmail.com>
2025-04-30 02:00:22 +0600

    api call improvement

--- src/components/AnnouncementBar/AnnouncementBar.tsx
+++ src/components/AnnouncementBar/AnnouncementBar.tsx
@@ -11,6 +11,30 @@ interface AnnouncementBarProps {
  * Displays a list of announcements with optional links.
  */
 const AnnouncementBar: React.FC<AnnouncementBarProps> = ({ className }) => {
+  /*
+  use when fetching data from an API
+  */
+  // const [announcementData, setAnnouncementData] = useState<AnnouncementBarProps[]>([]);
+  // const [error, setError] = useState<string | null>(null);
+  // useEffect(() => {
+  //   const fetchAnnouncementData = async () => {
+  //     try{
+  //       const response = await fetch("/data/announcementBarData.json");
+  //       if (!response.ok) {
+  //         throw new Error("Network response was not ok");
+  //       }
+  //       const data = await response.json();
+  //       setAnnouncementData(data);
+  //     } catch (err) {
+  //       setError(String(err));
+  //       console.error("Error fetching announcement data:", err);
+  //     }
+  //   };
+  //   fetchAnnouncementData();
+  // }, []);
+  // if (error) {
+  //   return <div className="text-red-500">Error: {error}</div>;
+  // }
   return (
     <div
       className={`announcement-bar text-center flex justify-center items-center gap-3 p-5 md:py-2.5 ${className || ""}`}
